b38cc8218da1126cd1125f1ab3cec590d3315863
Kishor Pingale <kishorpingale@gmail.com>
2018-12-17 13:37:52 +0530

    Firebase integrate

--- index.js
+++ index.js
@@ -4,4 +4,17 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+// Initialize Firebase
+import * as firebase from 'firebase';  
+  var fireBaseconfig = {
+    apiKey: "AIzaSyBFSJKGr09r7ClLm7nAwnaYCYTjilJZpwc",
+    authDomain: "firstreactproject-7ef22.firebaseapp.com",
+    databaseURL: "https://firstreactproject-7ef22.firebaseio.com",
+    projectId: "firstreactproject-7ef22",
+    storageBucket: "firstreactproject-7ef22.appspot.com",
+    messagingSenderId: "780705490129"
+  };
+  // firebase.initializeApp(fireBaseconfig);
+const firebaseApp = firebase.initializeApp(fireBaseconfig);
+
 AppRegistry.registerComponent(appName, () => App);
